e016fd593e40b4b8fa303679abb09e6ac4c492ca
LFLATH <ludwiglechtreck@gmail.com>
2024-05-21 13:04:44 -0400

    Command Line arguments

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("First Commit")
+	fileName := os.Args[1]
+	if fileName == "" {
+		fmt.Println("Please provide a file")
+	} else {
+		fmt.Println(fileName)
+	}
 }
